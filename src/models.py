60cf4c73902e157a8b45ea49226dccd09c03192b
MicaelaDeRosa23 <micaaderosa@gmail.com>
2024-11-20 05:06:00 +0000

    MicaelaDeRosa.StarWars

--- src/models.py
+++ src/models.py
@@ -14,33 +14,93 @@ class User(Base):
     id = Column(Integer, primary_key=True)
     username = Column(String,unique=True, nullable=False)
     password=Column(String,nullable=False)
+    likes=relationship("likes",back_populates="user")
+
+
+class Likes(Base):
+    __tablename__ ="favorites"
+    id = Column(Integer,primary_key=True)
+    user_id = Column(Integer,ForeignKey("users.id"))
+    planet_id = Column(Integer,ForeignKey("planets.id"))
+    vehicle_id = Column(Integer,ForeignKey("vehicles.id"))
+    character_id = Column(Integer,ForeignKey("people.id"))
+    film_id = Column(String,ForeignKey("species.id"))
+    user = relationship("user",back_populates="likes")
+    planet = relationship("Planet")
+    character = relationship("Person")
+    vehicle = relationship("Vehicle")
+    starship = relationship("Starship")
+    film = relationship("Film")
+
+class Vehicle(Base):
+    __tablename__ ="vehicles"
+    id =Column(Integer,primary_key=True)
+    name = Column(String)
+    model = Column(String)
+    manufacturer = Column(String)
+    cost_in_credits = Column(String)
+    vehicle_class = Column(String)
+    pilots = Column(String,ForeignKey("people.id"))   
+    films = Column(String,ForeignKey("films.id"))    
+
+class Species(Base):
+    __tablename__ = 'species'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    classification = Column(String)
+    designation = Column(String)
+    average_height = Column(String)
+    average_lifespan = Column(String)
+    hair_colors = Column(String)
+    homeworld = Column(Integer, ForeignKey('planets.id'))
+    people = Column(String, ForeignKey('people.id'))
 
 class Film(Base):
     __tablename__ ="films"
     id =Column(Integer,primary_key=True)
-    Name = Column(String)
-    Duration = Column(String)
-    Director = Column(String)
-    planets=Column(Integer,ForeignKey("planets.id"))
-    vehicles=Column(Integer,ForeignKey("vehicles.id"))
-    characters=Column(Integer,ForeignKey("person.id"))
+    title = Column(String)
+    director = Column(String)
+    opening_crawl = Column(String)
+    episode_id = Column(Integer)
+    planets = Column(Integer,ForeignKey("planets.id"))
+    vehicles = Column(Integer,ForeignKey("vehicles.id"))
+    characters = Column(Integer,ForeignKey("people.id"))
+    species = Column(String,ForeignKey("species.id"))
+    starships = Column(String,ForeignKey("starships.id"))
+
 
 class Planet(Base):
     __tablename__ ="planets"
     id =Column(Integer,primary_key=True)
     population = Column(String)
-    clime = Column(String)
-    color = Column(String)
+    diameter = Column(String)
+    terrain = Column(String)
     name = Column(String)
-    homeworld=Column(Integer,ForeignKey("planets.id"))
+    rotation_period=Column(String)
 
-class Vehicle(Base):
-    __tablename__ ="vechicles"
-    id =Column(Integer,primary_key=True)
-    name = Column(String)
+class Starships(Base):
+    __tablename__ = 'starships'
+    id = Column(Integer, primary_key=True)
     model = Column(String)
-    color = Column(String)
-    pilots = Column(String,ForeignKey("person.id"))
+    starship_class = Column(String)
+    manufacturer = Column(String)
+    cost_in_credits = Column(String)
+    lenght = Column(String)
+    passengers = Column(String)
+    pilots = Column(String, ForeignKey('people.id'))
+    name = Column (String)
+
+class Person(Base):
+    __tablename__ = 'people'
+    id = Column(Integer, primary_key=True)
+    height = Column(String)
+    mass = Column(String)
+    hair_color = Column(String)
+    skin_color = Column(String)
+    birth_year = Column(String)
+    gender = Column(String)
+    name = Column(String)
+    homeworld = Column(Integer, ForeignKey('planets.id'))
 
 
 
